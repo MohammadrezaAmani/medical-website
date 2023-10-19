5ccf7dbd4e7a432abca1abd81cabb50130a54b94
MohammadrezaAmani <more.amani@yahoo.com>
2023-10-19 16:00:11 +0330

    add doctor sessions

--- doctor/urls.py
+++ doctor/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .views import DoctorListCreateView, DoctorDetailView, DoctorExercises
+from .views import (
+    DoctorListCreateView,
+    DoctorDetailView,
+    DoctorExercises,
+    DoctorPatients,
+    DoctorSessions,
+)
 
 urlpatterns = [
     path("", DoctorListCreateView.as_view(), name="doctor-list"),
     path("<int:pk>/", DoctorDetailView.as_view(), name="doctor-detail"),
     path("<int:pk>/exercises/", DoctorExercises.as_view(), name="doctor-exercises"),
+    path("<int:pk>/patients/", DoctorPatients.as_view(), name="doctor-patients"),
+    path("<int:pk>/sessions/", DoctorSessions.as_view(), name="doctor-sessions"),
 ]
--- doctor/views.py
+++ doctor/views.py
@@ -3,6 +3,9 @@ from .models import Doctor
 from .serializers import DoctorSerializer
 from exercise.serializers import ExerciseSerializer
 from rest_framework.response import Response
+from patient.serializers import PatientSerializer
+from session.serializers import SessionSerializer
+
 
 class DoctorListCreateView(generics.ListCreateAPIView):
     queryset = Doctor.objects.all()
@@ -13,6 +16,7 @@ class DoctorDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
 
+
 class DoctorExercises(generics.RetrieveAPIView):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
@@ -21,4 +25,26 @@ class DoctorExercises(generics.RetrieveAPIView):
         doctor = self.get_object()
         exercises = doctor.exercise_set.all()
         serializer = ExerciseSerializer(exercises, many=True)
+        return Response(serializer.data)
+
+
+class DoctorPatients(generics.RetrieveAPIView):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
+
+    def get(self, request, *args, **kwargs):
+        doctor = self.get_object()
+        patients = doctor.patient_set.all()
+        serializer = PatientSerializer(patients, many=True)
+        return Response(serializer.data)
+
+
+class DoctorSessions(generics.RetrieveAPIView):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
+
+    def get(self, request, *args, **kwargs):
+        doctor = self.get_object()
+        sessions = doctor.session_set.all()
+        serializer = SessionSerializer(sessions, many=True)
         return Response(serializer.data)
