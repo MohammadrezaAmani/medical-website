cd6c1e23ed1b7a8ae978c54832cef075671d7902
MohammadrezaAmani <more.amani@yahoo.com>
2023-10-19 15:01:16 +0330

    add doctor urls

---
+++ session/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from .models import Doctor
+class DoctorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Doctor
+        fields = '__all__'
--- session/views.py
+++ session/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Doctor
+from .serializers import DoctorSerializer
 
-# Create your views here.
+class DoctorListCreateView(generics.ListCreateAPIView):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
+
+class DoctorDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
