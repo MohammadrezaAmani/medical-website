26565ea30cff684c109ddc7366c6da10c04ab477
MohammadrezaAmani <more.amani@yahoo.com>
2023-10-19 15:34:19 +0330

    class PatientSessions

--- patient/views.py
+++ patient/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics
 from .models import Patient
 from .serializers import PatientSerializer
-
+from rest_framework.response import Response
+from session.serializers import SessionSerializer
 
 class PatientListCreateView(generics.ListCreateAPIView):
     queryset = Patient.objects.all()
@@ -11,3 +12,13 @@ class PatientListCreateView(generics.ListCreateAPIView):
 class PatientDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
+
+class PatientSessions(generics.RetrieveAPIView):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
+
+    def get(self, request, *args, **kwargs):
+        patient = self.get_object()
+        sessions = patient.session_set.all()
+        serializer = SessionSerializer(sessions, many=True)
+        return Response(serializer.data)
