1a51eeb48024d58b3105796331936f5eb15d3aa7
MohammadrezaAmani <more.amani@yahoo.com>
2023-10-21 03:23:38 +0330

    update admin page

--- exercise/admin.py
+++ exercise/admin.py
@@ -1,16 +1,94 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import (
     Equipment,
     Goal,
     Displacement,
     PlacementPosition,
+    Target,
+    Organ,
     Exercise,
 )
 
-admin.site.register(Exercise)
-admin.site.register(Equipment)
-admin.site.register(Goal)
-admin.site.register(Displacement)
-admin.site.register(PlacementPosition)
+
+@admin.register(Equipment)
+class EquipmentAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Goal)
+class GoalAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Displacement)
+class DisplacementAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(PlacementPosition)
+class PlacementPositionAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Target)
+class TargetAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Organ)
+class OrganAdmin(admin.ModelAdmin):
+    list_display = ("name", "photo")
+    search_fields = ("name",)
+    list_filter = ("name",)
+
+
+@admin.register(Exercise)
+class ExerciseAdmin(admin.ModelAdmin):
+    list_display = ("name", "owner", "is_public")
+    list_filter = ("owner", "is_public")
+    search_fields = ("name", "description")
+    filter_horizontal = (
+        "placement_position",
+        "target",
+        "target_organs",
+        "displacement",
+        "accessories",
+    )
+
+    fieldsets = (
+        (
+            "General Information",
+            {
+                "fields": (
+                    "owner",
+                    "name",
+                    "description",
+                    "instructions",
+                    "is_public",
+                    "keywords",
+                )
+            },
+        ),
+        ("Media", {"fields": ("video", "photo")}),
+        (
+            "Related Information",
+            {
+                "fields": (
+                    "placement_position",
+                    "target",
+                    "target_organs",
+                    "displacement",
+                    "accessories",
+                )
+            },
+        ),
+    )
+
+
+# admin.site.site_header = "Your Admin Site Header"  # Customize the admin site header
+# admin.site.site_title = "Your Admin Site Title"  # Customize the admin site title
