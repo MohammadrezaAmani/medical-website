80df41eb95a72d0da7605c5b8527688516f68841
MohammadrezaAmani <more.amani@yahoo.com>
2023-10-20 17:38:59 +0330

    update patient model

--- doctor/models.py
+++ doctor/models.py
@@ -28,32 +28,3 @@ class Doctor(models.Model):
 
     def get_absolute_url(self):
         return reverse("Doctor_detail", kwargs={"pk": self.pk})
-
-
-class Patient(models.Model):
-    user = models.OneToOneField(
-        User, on_delete=models.CASCADE, related_name="patient_profile"
-    )
-    GENDER_OPTIONS = (("m", "Male"), ("f", "Female"))
-    name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    photo = models.ImageField(upload_to="images/", null=True, blank=True)
-    bio = models.TextField(blank=True, null=True)
-    gender = models.CharField(max_length=1, choices=GENDER_OPTIONS)
-    birth_date = models.DateField()
-    address = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=10)
-    insurance_number = models.CharField(max_length=10)
-    insurance_company = models.CharField(max_length=100)
-    injury_date = models.DateField()
-    injury_description = models.CharField(max_length=100)
-    injury_type = models.CharField(max_length=100)
-    medical_documents = models.FileField(upload_to="documents/", null=True, blank=True)
-    is_active = models.BooleanField(default=False)
-    is_doctor = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse("Patient_detail", kwargs={"pk": self.pk})
--- patient/models.py
+++ patient/models.py
@@ -1,11 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.urls import reverse
 from doctor.models import Doctor
 
 
 class Patient(models.Model):
-    # user = models.OneToOneField(BaseUser, on_delete=models.CASCADE, primary_key=True)
-    GENDER_OPTIONS = (("m", "male"), ("f", "female"))
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name="patient_profile"
+    )
+    doctor = models.ForeignKey(
+        Doctor, on_delete=models.CASCADE, related_name="patients"
+    )
+    GENDER_OPTIONS = (("m", "Male"), ("f", "Female"))
     name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     photo = models.ImageField(upload_to="images/", null=True, blank=True)
@@ -14,17 +20,17 @@ class Patient(models.Model):
     birth_date = models.DateField()
     address = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=10)
-    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
     insurance_number = models.CharField(max_length=10)
     insurance_company = models.CharField(max_length=100)
     injury_date = models.DateField()
     injury_description = models.CharField(max_length=100)
     injury_type = models.CharField(max_length=100)
     medical_documents = models.FileField(upload_to="documents/", null=True, blank=True)
-    password = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=False)
+    is_doctor = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
-        return reverse("Doctor_detail", kwargs={"pk": self.pk})
+        return reverse("Patient_detail", kwargs={"pk": self.pk})
