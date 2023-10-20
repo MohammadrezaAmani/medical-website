4809733bc3cea438efde0bba60af3e0dffa9b703
MohammadrezaAmani <more.amani@yahoo.com>
2023-10-21 03:14:53 +0330

    minor changes

--- doctor/views.py
+++ doctor/views.py
@@ -7,8 +7,6 @@ from rest_framework import status, generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
-
-# pagination
 from rest_framework.pagination import PageNumberPagination
 from rest_framework_simplejwt.tokens import RefreshToken
 from exercise.serializers import ExerciseSerializer
