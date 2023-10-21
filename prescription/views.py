f413429dbd542cdae44e48f38add59055de17aac
MohammadrezaAmani <more.amani@yahoo.com>
2023-10-21 05:32:37 +0330

    add premissions

--- prescription/views.py
+++ prescription/views.py
@@ -1,3 +1,4 @@
+from django.db.models.query import QuerySet
 from rest_framework import generics
 from .models import Prescription, Drug
 from .serializers import PrescriptionSerializer, DrugSerializer
@@ -11,18 +12,64 @@ from doctor.views import (
 class PrescriptionListCreateView(generics.ListCreateAPIView):
     queryset = Prescription.objects.all()
     serializer_class = PrescriptionSerializer
+    
+    def get_queryset(self) -> QuerySet:
+        user = get_user_from_token(self.request)
+        if user.is_doctor:
+            doctor = get_doctor_from_token(self.request)
+            return Prescription.objects.filter(patient__doctor=doctor)
+        elif user.is_patient:
+            patient = get_patient_from_token(self.request)
+            return Prescription.objects.filter(patient=patient)
+        else:
+            return Prescription.objects.none()
 
 
 class PrescriptionDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Prescription.objects.all()
     serializer_class = PrescriptionSerializer
 
+    def get_queryset(self) -> QuerySet:
+        user = get_user_from_token(self.request)
+        if user.is_doctor:
+            doctor = get_doctor_from_token(self.request)
+            return Prescription.objects.filter(patient__doctor=doctor)
+        elif user.is_patient:
+            patient = get_patient_from_token(self.request)
+            return Prescription.objects.filter(patient=patient)
+        else:
+            return Prescription.objects.none()
+
 
 class DrugListCreateView(generics.ListCreateAPIView):
     queryset = Drug.objects.all()
     serializer_class = DrugSerializer
+    
+    def get_queryset(self) -> QuerySet:
+        user = get_user_from_token(self.request)
+        if user.is_doctor:
+            doctor = get_doctor_from_token(self.request)
+            return Drug.objects.filter(patient__doctor=doctor)
+        elif user.is_patient:
+            patient = get_patient_from_token(self.request)
+            return Drug.objects.filter(patient=patient)
+        else:
+            return Drug.objects.none()
+        
 
 
 class DrugDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Drug.objects.all()
     serializer_class = DrugSerializer
+    
+    def get_queryset(self) -> QuerySet:
+        user = get_user_from_token(self.request)
+        if user.is_doctor:
+            doctor = get_doctor_from_token(self.request)
+            return Drug.objects.filter(patient__doctor=doctor)
+        elif user.is_patient:
+            patient = get_patient_from_token(self.request)
+            return Drug.objects.filter(patient=patient)
+        else:
+            return Drug.objects.none()
+        
