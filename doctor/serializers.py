d88af05f1ec23d8273fca3f47688ca2202d4b32b
MohammadrezaAmani <more.amani@yahoo.com>
2023-10-26 23:49:19 +0000

    minor changes.

--- doctor/serializers.py
+++ doctor/serializers.py
@@ -3,11 +3,19 @@ from doctor.models import Doctor
 
 
 class DoctorSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Doctor model.
+    Excludes password field from serialization.
+    """
     class Meta:
         model = Doctor
         exclude = ("password",)
 
 
 class DoctorLoginSerializer(serializers.Serializer):
+    """
+    Serializer for Doctor login.
+    Contains username and password fields.
+    """
     username = serializers.EmailField(max_length=100)
     password = serializers.CharField(max_length=100)
