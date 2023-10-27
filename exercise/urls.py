a4ac471f36f83335052be9e1b5425cf0cd3dd382
MohammadrezaAmani <more.amani@yahoo.com>
2023-10-27 00:24:44 +0000

    update urls

--- exercise/urls.py
+++ exercise/urls.py
@@ -1,3 +1,28 @@
+"""
+This module contains the URL patterns for the exercise app.
+
+The urlpatterns variable is a list of URL patterns. Each URL pattern is defined using the path() function
+from Django's urls module. The path() function takes two required arguments: route and view, and several
+optional arguments. The route argument is a string that contains a URL pattern. The view argument is a
+callable that takes a request object as its argument and returns an HTTP response.
+
+This module defines the following URL patterns:
+- equipment/ : A URL pattern that maps to EquipmentListCreateView.
+- equipment/<int:pk>/ : A URL pattern that maps to EquipmentDetailView.
+- goals/ : A URL pattern that maps to GoalListCreateView.
+- goals/<int:pk>/ : A URL pattern that maps to GoalDetailView.
+- displacements/ : A URL pattern that maps to DisplacementListCreateView.
+- displacements/<int:pk>/ : A URL pattern that maps to DisplacementDetailView.
+- placement-positions/ : A URL pattern that maps to PlacementPositionListCreateView.
+- placement-positions/<int:pk>/ : A URL pattern that maps to PlacementPositionDetailView.
+- targets/ : A URL pattern that maps to TargetListCreateView.
+- targets/<int:pk>/ : A URL pattern that maps to TargetDetailView.
+- organs/ : A URL pattern that maps to OrganListCreateView.
+- organs/<int:pk>/ : A URL pattern that maps to OrganDetailView.
+- '' : A URL pattern that maps to ExerciseListView.
+
+"""
+
 from django.urls import path
 from .views import (
     EquipmentListCreateView,
